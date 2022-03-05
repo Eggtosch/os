e6aa311075c77e8082ce0d8f377df159725c23ae
Oskar <oskar.munz@gmx.de>
2022-03-05 02:49:45 +0100

    add basic key printing functionality

--- kernel/device/keyboard.c
+++ kernel/device/keyboard.c
@@ -3,20 +3,35 @@
 #include <device/pic.h>
 #include <os/io.h>
 #include <debug.h>
+#include <stdio.h>
 
 
 #define KEYBOARD_DATA (0x60)
 #define KEYBOARD_CMD  (0x64)
 
 
-//static char scancodes[128] = {
-//};
+static char _scancodes[] = {
+	0, 0,
+	'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '?', '`', '\b',
+	'\t','q', 'w', 'e', 'r', 't', 'z', 'u', 'i', 'o', 'p', 'u', '+', '\n',
+	0, 'a', 's', 'd', 'f', 'g', 'h', 'j', 'k', 'l', 'o', 'a', '<',
+	0, '#', 'y', 'x', 'c', 'v', 'b', 'n', 'm', ',', '.', '-', 0,
+	0, 0, ' ', 0, ' ', 0, 0, 0, 0, 0, 0,
+	[0x56] = '<'
+};
 
 
 static void isr_keyboard(struct cpu_state *cpu_state) {
 	(void) cpu_state;
 	u8 scancode = io_inb(KEYBOARD_DATA);
-	debug(DEBUG_INFO, "got keyboard interrupt %#x", scancode);
+	char key = keyboard_scancode_to_ascii(scancode);
+	if (!(scancode & 0x80)) {
+		printf("%c", key);
+	}
+}
+
+char keyboard_scancode_to_ascii(u8 scancode) {
+	return _scancodes[scancode & 0x7f];
 }
 
 void keyboard_init(void) {
--- kernel/device/keyboard.h
+++ kernel/device/keyboard.h
@@ -1,3 +1,6 @@
 #pragma once
 
+#include <common.h>
+
 void keyboard_init(void);
+char keyboard_scancode_to_ascii(u8 scancode);
