104a2726e8bbbd3afc4ce4f1ec1a1ce4145d0258
Oskar Munz <oskar.munz@gmx.de>
2023-03-18 02:04:39 +0100

    Initialize hpet pit with higher value if boot process takes long time

--- kernel/process/scheduler.c
+++ kernel/process/scheduler.c
@@ -4,6 +4,7 @@
 #include <memory/vmm.h>
 #include <acpi/hpet.h>
 #include <io/stdio.h>
+#include <driver/util.h>
 
 extern void asm_jump_usermode(u64 addr, u64 stack, u64 prog, u64 size);
 
@@ -29,6 +30,14 @@ void scheduler_init(void) {
 }
 
 void scheduler_enable(bool enable) {
+	i64 secs = time_since_boot();
+	u64 ns = time_current_ns();
+	u64 missed_ints = 4 * secs + ns / 250000000;
+	for (u64 i = 0; i < missed_ints; i++) {
+		hpet_next_timeout(HPET_TIMER_PIT);
+	}
+	kprintf("catching up %d scheduler interrupts\n", missed_ints);
+
 	interrupt_enable(INT_PIT, enable);
 	kprintf("%sabling scheduler timer interrupts\n", enable ? "en" : "dis");
 }
