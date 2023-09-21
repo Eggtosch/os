f8653d4eed90e4707d0a63b045f38dae5daae20e
Oskar Munz <oskar.munz@gmx.de>
2023-09-21 23:09:03 +0200

    update documentation for libc syscalls

--- libc/include/syscall.h
+++ libc/include/syscall.h
@@ -2,65 +2,47 @@
 
 #include <common.h>
 
-/* syscall 0x0:
- * @params
- * rax = syscall number
- * rbx = name
- * rcx = flags
- * @return
- * rax = file descriptor, negative if invalid
+/* syscall 0x00: open a file.
+ * name: file path.
+ * flags: unused at the moment.
+ * return: file descriptor, negative if invalid.
  */
 int sys_open(const char *name, u64 flags);
 
-/* syscall 0x1:
- * @params
- * rax = syscall number
- * rbx = fd
- * rcx = buffer
- * rdx = count
- * @return
- * rax = number of bytes read
+/* syscall 0x01: read bytes from a file.
+ * fd: file descriptor.
+ * buffer: where to read into.
+ * count: number of bytes to read.
+ * return: number of bytes actually read.
  */
 u64 sys_read(int fd, void *buffer, u64 count);
 
-/* syscall 0x2:
- * @params
- * rax = syscall number
- * rbx = fd
- * rcx = buffer
- * rdx = count
- * @return
- * rax = number of bytes written
+/* syscall 0x02: write bytes to a file.
+ * fd: file descriptor.
+ * buffer: data to write.
+ * count: number of bytes to write.
+ * return: number of bytes actually written.
  */
 u64 sys_write(int fd, void *buffer, u64 count);
 
-/* syscall 0x3
- * @params
- * rax = syscall number
- * rbx = addr (start addr of new mapping)
- * rcx = length (length of new mapping in bytes)
- * rdx = fd (optionally map data from fd, -1 if none)
- * rdi = flags
- * @return
- * rax = indication of success, negative if error occured
+/* syscall 0x03: map memory.
+ * addr: virtual address of new mapping.
+ * length: length of new mapping in bytes.
+ * fd: optionally map data from fd, -1 if none.
+ * flags: unused at the moment.
+ * return: status, negative if error occured.
  */
 int sys_mmap(void *addr, u64 length, int fd, u64 flags);
 
-/* syscall 0x4
- * @params
- * rax = syscall number
- * rbx = addr (addr of mapping)
- * rcx = length (length of mapping)
- * @return
- * rax = indication of success, negative if error occured
+/* syscall 0x04: unmap memory.
+ * addr: virtual address of mapping.
+ * length: length of mapping.
+ * return: status, negative if error occured.
  */
 int sys_munmap(void *addr, u64 length);
 
 /*
- * syscall 0x5
- * @params
- * rax = syscall number
- * @return -
+ * syscall 0x05: exit the calling process.
  */
 void sys_exit(void);
 
